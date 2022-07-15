5ce7837d11d53f4b922c382c3629043246bddbe5
zlepper <rhdh@digizuite.com>
2022-07-15 14:21:22 +0200

    Add a bit more branch info to build number when a concrete version number cannot be extracted

--- index.mjs
+++ index.mjs
@@ -3,18 +3,33 @@ import core from '@actions/core'
 const branchVersionRegex = /^.*?\/(?<version>\d+\.\d+\.\d+)(?:\/(?:.*)+)?$/;
 const versionTagRegex = /^v(?<version>\d+\.\d+\.\d+)$/;
 
+function getBranchInfoNumber(refName) {
+    return refName.replaceAll(/"<>\|\t\\/, '').replaceAll('/', "_");
+}
+
 function extractVersionNumber() {
     const refName = process.env.GITHUB_REF_NAME;
+    const runNumber = process.env.GITHUB_RUN_NUMBER;
     const result = branchVersionRegex.exec(refName) ?? versionTagRegex.exec(refName);
     let version;
     if (result === null) {
         version = core.getInput('main_version');
+
+        if(refName !== 'master') {
+            core.info('Not on master branch, adding branch name to build name');
+            version += "-" + getBranchInfoNumber(refName);
+        }
+
         core.info(`Version number was not found in branch name, assuming latest. Got RefName: '${refName}'. Falling back to main version: '${version}'`);
     } else {
         version = result.groups.version;
+        if(!refName.startsWith('release/')) {
+            core.info('not on a strict release branch, adding more info to build number')
+            version += "-" + getBranchInfoNumber(refName);
+        }
         core.info(`Version was extracted from refname without issues: '${version}'`);
     }
-    return version;
+    return `${version}-${runNumber}`;
 }
 
 try {
