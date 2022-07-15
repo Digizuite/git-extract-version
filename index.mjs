13cc61799da6d322d85aaef3bac7daa3119d90ce
zlepper <rhdh@digizuite.com>
2022-07-15 08:02:04 +0200

    Minor cleanup

--- index.mjs
+++ index.mjs
@@ -1,19 +1,27 @@
 import core from '@actions/core'
 
-const branchVersionRegex = /^(?:.*?)\/(?<version>\d+\.\d+\.\d+)(?:\/(?:.*)+)?$/;
+const branchVersionRegex = /^.*?\/(?<version>\d+\.\d+\.\d+)(?:\/(?:.*)+)?$/;
 const versionTagRegex = /^v(?<version>\d+\.\d+\.\d+)$/;
-try {
 
+function extractVersionNumber() {
     const refName = process.env.GITHUB_REF_NAME;
     const result = branchVersionRegex.exec(refName) ?? versionTagRegex.exec(refName);
-    if(result === null) {
-        core.info(`Version number was not found in branch name, assuming latest. Got RefName: '${refName}'`)
-        core.setOutput('version', core.getInput('main_version'))
+    let version;
+    if (result === null) {
+        version = core.getInput('main_version');
+        core.info(`Version number was not found in branch name, assuming latest. Got RefName: '${refName}'. Falling back to main version: '${version}'`);
     } else {
-        const version = result.groups.version;
+        version = result.groups.version;
         core.info(`Version was extracted from refname without issues: '${version}'`);
-        core.setOutput('version', version);
     }
+    return version;
+}
+
+try {
+
+    const version = extractVersionNumber();
+
+    core.setOutput('version', version);
 
 } catch(error) {
     core.setFailed(error.message);
